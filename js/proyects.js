209a1c2a4daa80a7dec56db104ab06b02d27ce28
Nour._07 <91376968+MhdNourSendyan@users.noreply.github.com>
2022-05-14 15:32:31 +0200

    Add files via upload

---
+++ js/proyects.js
@@ -0,0 +1,32 @@
+const proyects = [
+    // {
+    //     name: 'Pintorrea',
+    //     description: 'Pintorrea is a multiplayer game where you can fun with your friends drawing.',
+    //     githubUrl: 'https://github.com/MiYazJE/Pintorrea',
+    //     url: 'https://pintorrea.herokuapp.com',
+    //     imagePath: ''
+    // },
+    // {
+    //     name: 'Pathfinding',
+    //     description: 'A pathfinding algorithm that search the best route between 2 points (Dijkstra).',
+    //     githubUrl: 'https://github.com/MiYazJE/Pathfinding-Visualization',
+    //     url: 'https://pathfinding-visualization.vercel.app/',
+    //     imagePath: ''
+    // },
+    // {
+    //     name: 'PokeReactive',
+    //     description: 'PokeReactive is a website for searching any pokemon, it uses a public api.',
+    //     githubUrl: 'https://github.com/MiYazJE/PokeReact',
+    //     url: 'https://poke-react-fawn.vercel.app/',
+    //     imagePath: ''
+    // },
+    // {
+    //     name: 'Video Share',
+    //     description: 'Video Share is a website to view youtube videos synchronously with friends.',
+    //     githubUrl: 'https://github.com/MiYazJE/Share-Videos',
+    //     url: 'http://videowithme.herokuapp.com/',
+    //     imagePath: ''
+    // },
+];
+
+export { proyects };
